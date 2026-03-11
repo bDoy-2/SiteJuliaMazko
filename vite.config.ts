267fc4250bf5dfefcf9ecadc8d2a3424e5d58252
bDoy-2 <bodyabodya940@gmail.com>
2026-03-11 21:30:18 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import runableWebsiteRuntime from "runable-website-runtime"
 import runableAnalyticsPlugin from "./vite/plugins/runable-analytics-plugin";
 
 export default defineConfig({
-	base: './',
+	base: '/SiteJuliaMazko/',
 	plugins: [react(), runableAnalyticsPlugin(), runableWebsiteRuntime(), cloudflare(), tailwind()],
 	resolve: {
 		alias: {
